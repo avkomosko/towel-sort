0361c1092d1a26c20d42e0d39d641336ef6d38c7
Aliaksei <avkomosko@gmail.com>
2021-08-22 19:03:02 +0300

    feat: implement function and finish task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort(matrix) {
-    return [];
+    let result =[];
+    let arr =[];
+    if (arguments.length !== 0 && matrix.length !== 0) {
+        for (let i=0; i< matrix.length; i++){
+            if (i % 2 === 0 ) {
+                arr.push(matrix[i].sort((a,b)=> a-b));
+            } else {
+                arr.push(matrix[i].sort((a,b)=> b-a));
+            }
+            result =  result.concat(arr[i]);
+        }
+    }
+    return result;
 };
